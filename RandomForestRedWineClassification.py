444ddc4241fd98601659ed49c566f946b5d0356f
prajyotnaik3 <56463673+prajyotnaik3@users.noreply.github.com>
2020-10-13 11:27:28 +0530

    Wine quality classification in three types

---
+++ RandomForestRedWineClassification.py
@@ -0,0 +1,52 @@
+#Imports
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.preprocessing import StandardScaler, Normalizer, MinMaxScaler
+
+#Read the dataset
+dataset = pd.read_csv(r'data/winequality-red.csv')
+#print(dataset.head(5))
+
+#NA values in the dataset
+#print("Count of NA values:")
+#print(dataset.isna().sum())
+
+print(set(dataset['quality']))
+
+dataset['quality'] = dataset['quality'].map({
+        3 : 0,
+        4 : 0,
+        5 : 1,
+        6 : 1,
+        7 : 2,
+        8 : 2
+        })
+
+X = dataset.iloc[:, :11].values
+y = dataset.iloc[:, [11]].values
+
+#Split data into training and testing split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0, stratify = y)
+
+#Preprocess the dataset
+#scaler = StandardScaler()
+#scaler = Normalizer()
+#scaler = MinMaxScaler()
+#X_train = scaler.fit_transform(X_train)
+#X_test = scaler.transform(X_test)
+
+#Train a classifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'gini', random_state = 0)
+classifier.fit(X_train, y_train.ravel())
+
+#Predcitions
+y_pred = classifier.predict(X_test)
+
+#Results
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion matrix:")
+print(cm)
+print("Accuracy:", accuracy_score(y_test, y_pred))
--- SVMKernelWhiteWineClassification.py
+++ SVMKernelWhiteWineClassification.py
@@ -1,28 +1,33 @@
 #Imports
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.preprocessing import StandardScaler, Normalizer, MinMaxScaler
 
 #Read the dataset
-dataset1 = pd.read_csv(r'data/winequality-red.csv')
-dataset2 = pd.read_csv(r'data/winequality-white.csv')
-print(dataset1.head(5))
-print(dataset2.head(5))
+dataset = pd.read_csv(r'data/winequality-white.csv')
+#print(dataset.head(5))
 
 #NA values in the dataset
-print("Count of NA values:")
-print(dataset1.isna().sum())
-print(dataset2.isna().sum())
+#print("Count of NA values:")
+#print(dataset.isna().sum())
 
-dataset1['Class'] = ['red']*dataset1.shape[0]
-dataset2['Class'] = ['white']*dataset2.shape[0]
+print(set(dataset['quality']))
 
-dataset = pd.concat([dataset1, dataset2], axis = 0)
-#Filter independent and dependent variables
-X = dataset.iloc[:, :12].values
-y = dataset.iloc[:, [12]].values
+dataset['quality'] = dataset['quality'].map({
+        3 : 0,
+        4 : 0,
+        5 : 1,
+        6 : 1,
+        7 : 2,
+        8 : 2,
+        9 : 2
+        })
+
+X = dataset.iloc[:, :11].values
+y = dataset.iloc[:, [11]].values
 
 #Split data into training and testing split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0, stratify = y)
@@ -35,7 +40,7 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 #Train a classifier
-classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5), activation = 'relu', random_state = 0)
+classifier = SVC(kernel = 'rbf', random_state = 0, gamma = 'auto')
 classifier.fit(X_train, y_train.ravel())
 
 #Predcitions
